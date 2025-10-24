eb86a20479e5b3ecf2e9a131142b2ac1685e4509
nekonoshiri <gexira.halen.toms@gmail.com>
2025-10-24 15:43:08 +0900

    wip

--- index.js
+++ index.js
@@ -31,12 +31,47 @@ const plugin = {
     },
   },
 
+  オプション付テンプレートコンパイル: {
+    // テンプレートをオプション付きでコンパイルして返します。コンパイルされたテンプレートは「テンプレート評価」関数に渡すことで評価（展開）できます。 // @おぷしょんつきてんぷれーとこんぱいる
+    type: "func",
+    josi: [["を"], ["の"]],
+    fn: function (template, options) {
+      return Handlebars.compile(template, {
+        data: options?.データ使用許可,
+        compat: options?.互換モード,
+        knownHelpers: options?.既知ヘルパー,
+        knownHelpersOnly: options?.既知ヘルパー限定,
+        noEscape: options?.HTMLエスケープ無効化,
+        strict: options?.厳格モード,
+        assumeObjects: options?.オブジェクト存在確認無効化,
+        preventIndent: options?.自動インデント無効化,
+        ignoreStandalone: options?.スタンドアロンタグ空白除去無効化,
+        explicitPartialContext: options?.パーシャル暗黙コンテキスト無効化,
+        ...options,
+      });
+    },
+  },
+
   テンプレート評価: {
     // コンパイルされたテンプレートを評価（展開）して結果を返します。 // @てんぷれーとひょうか
     type: "func",
     josi: [["を"], ["で"]],
     fn: function (template, context) {
       return template(context);
+    },
+  },
+
+  オプション付テンプレート評価: {
+    // コンパイルされたテンプレートをオプション付きで評価（展開）して結果を返します。 // @おぷしょんつきてんぷれーとひょうか
+    type: "func",
+    josi: [["を"], ["で"], ["の"]],
+    fn: function (template, context, options) {
+      return template(context, {
+        data: options?.データ,
+        helpers: options?.ヘルパー,
+        partials: options?.パーシャル,
+        ...options,
+      });
     },
   },
 };
